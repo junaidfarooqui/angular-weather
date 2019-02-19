2f6b49088216c9db5449c15b245ecb668158b296
junaidfarooqui <junaidfarooqui61@gmail.com>
2019-02-19 08:51:08 +0100

    setting component and weather services added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { SettingComponent } from './setting/setting.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SettingComponent
   ],
   imports: [
     BrowserModule,
